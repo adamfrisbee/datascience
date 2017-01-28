d4a30090c0c6960a2ef512ab178844021b8257e5
adamfrisbee <adamfrisbee@gmail.com>
2017-01-28 10:46:43 -0700

    Small changes2

--- C2W2/corr.R
+++ C2W2/corr.R
@@ -1,14 +1,14 @@
 corr <- function(directory, threshold = 0) {
-  files_list <- list.files(directory, full.names=TRUE) #reads a directory of files
+  files_list <- list.files(directory, full.names=TRUE) #Reads a directory of files
   
-  cor_vector <- vector(mode = "numeric", length = 0)
+  cor_vector <- vector(mode = "numeric", length = 0) #Creates an empty vector
   
   for(each in 1:332) {
     tempdata <- read.csv(files_list[each])
     tempdata_complete <- complete.cases(tempdata)
     counter <- sum(tempdata_complete)
     if(counter>threshold) {
-      cor_vector <- cor(tempdata[["nitrate"]], tempdata[["sulfate"]])
+      cor_vector <- cor(tempdata[["nitrate"]], tempdata[["sulfate"]], use = "complete.obs")
     }
   }
   
